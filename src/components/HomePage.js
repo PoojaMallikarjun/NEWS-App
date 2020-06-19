7e701cbf694d89e295c74a5d318072bc969eb60a
Rakshitha <rakshitharavi2299@gmail.com>
2020-06-19 17:31:18 +0530

    Feat: Entertainment,Sports,Technology

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,29 +4,67 @@ import {
   Nav,
   NavItem,
   Navbar,
-  Row
+  Row,
+  Col,
+  Collapse,
+  NavbarToggler
 } from 'reactstrap';
 
 class HomePage extends Component {
+  
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      isNavOpen : false
+    };
+
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  toggleNav(){
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
+
   render() {
     return (
       <div>
-        <Navbar dark>
+        <Navbar dark expand="md" >
           <div className='container'>
-            <Nav>
-              <Row>
-                <NavItem>
-                    <NavLink className='nav-link' to='/topheadlines'>
-                      HeadLines
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink className='nav-link' to='/business'>
-                      Business
-                    </NavLink>
-                </NavItem>
-              </Row>
-            </Nav>
+            <NavbarToggler onClick={this.toggleNav} />
+              <Collapse isOpen={this.state.isNavOpen} navbar>
+                <Nav>
+                  <Col>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/topheadlines'>
+                          HeadLines
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink className='nav-link' to='/business'>
+                          Business
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/entertainment'>
+                          Entertainment
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/sports'>
+                          Sports
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/technology'>
+                          Technology
+                        </NavLink>
+                    </NavItem>
+                   </Col> 
+                </Nav>
+              </Collapse>
           </div>
         </Navbar>
       </div>
@@ -35,3 +73,40 @@ class HomePage extends Component {
 }
 
 export default HomePage;
+// import React, { Component } from 'react';
+// import { NavLink } from 'react-router-dom';
+// import {
+//   Nav,
+//   NavItem,
+//   Navbar,
+//   Row
+// } from 'reactstrap';
+
+// class HomePage extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Navbar dark>
+//           <div className='container'>
+//             <Nav>
+//               <Row>
+//                 <NavItem>
+//                     <NavLink className='nav-link' to='/topheadlines'>
+//                       HeadLines
+//                     </NavLink>
+//                   </NavItem>
+//                   <NavItem>
+//                     <NavLink className='nav-link' to='/business'>
+//                       Business
+//                     </NavLink>
+//                 </NavItem>
+//               </Row>
+//             </Nav>
+//           </div>
+//         </Navbar>
+//       </div>
+//     );
+//   }
+// }
+
+// export default HomePage;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Headlines from './TopHeadLines';
 import HomePage from './HomePage';
 import Business from './Business'
+import Entertainment from './Entertainment';
+import Sports from './Sports';
+import Technology from './Technology';
 
 class Main extends Component {
   render() {
@@ -14,6 +17,9 @@ class Main extends Component {
             <Route path='/home' />
             <Route exact path='/topheadlines' component={Headlines} />
             <Route exact path='/business' component={Business} />
+            <Route exact path='/entertainment' component={Entertainment} />
+            <Route exact path='/sports' component={Sports} />
+            <Route exact path='/technology' component={Technology} />
             <Redirect to='/home' />
           </Switch>
         </BrowserRouter>
