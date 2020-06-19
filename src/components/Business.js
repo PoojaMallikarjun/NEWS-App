33e0772a57a733a9af8081f6a4e0e0a3bbb2eb61
nithish ravindra <nithishravindra8@gmail.com>
2020-06-19 10:56:27 +0530

    bare

---
+++ src/components/Business.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Link } from 'react-router-dom';
+
+class HeadLines extends Component {
+  state = {
+    data: [],
+  };
+
+  async componentDidMount() {
+    let url = 'http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=87e9b26b27db476fbd16ff7dd3f71677';
+    let res = await axios.get(url);
+    let data = await res.data.articles;
+
+    this.setState({
+      data
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Business</h1>
+        <Link to='/home'>Home</Link>
+
+        {this.state.data.map((item, index) => (
+          <div key={index}>
+            <li>
+              {item.title}
+              {item.author}
+            </li>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+export default HeadLines;
---
+++ src/components/HomePage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+  Nav,
+  NavItem,
+  Navbar,
+  Row
+} from 'reactstrap';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar dark>
+          <div className='container'>
+            <Nav>
+              <Row>
+                <NavItem>
+                    <NavLink className='nav-link' to='/topheadlines'>
+                      HeadLines
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className='nav-link' to='/business'>
+                      Business
+                    </NavLink>
+                </NavItem>
+              </Row>
+            </Nav>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
