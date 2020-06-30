44ac9e6995538d3971927227ff94214b160bcb77
nithish ravindra <nithishravindra8@gmail.com>
2020-06-30 18:49:49 +0530

    updated news component, handled query params

--- src/components/News.js
+++ src/components/News.js
@@ -7,17 +7,16 @@ import {
   CardBody,
   CardHeader,
   CardSubtitle,
-  Button,Row, CardGroup
+  Button,
+  Row,
+  CardGroup,
 } from "reactstrap";
-
-// function refreshPage(){
-//   window.location.reload()
-// }
 
 class News extends React.Component {
   state = {
     data: [],
     errMess: "",
+    pathname: "",
   };
 
   componentDidMount() {
@@ -27,6 +26,7 @@ class News extends React.Component {
       if (result.data.articles.length > 1) {
         this.setState({
           data: result.data.articles,
+          pathname: type,
         });
       } else {
         this.setState({
@@ -35,55 +35,70 @@ class News extends React.Component {
       }
     });
   }
-  
-  
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("ComponentDidUpdate");
+    let type = this.props.match.params.type;
+    let url = `http://newsapi.org/v2/top-headlines?country=in&category=${type}&apiKey=${process.env.REACT_APP_API_KEY}`;
+    if (type !== prevState.pathname) {
+      axios.get(url).then((result) => {
+        if (result.data.articles.length > 1) {
+          this.setState({
+            data: result.data.articles,
+            pathname: type,
+          });
+        } else {
+          this.setState({
+            errMess: "Page not found",
+          });
+        }
+      });
+    }
+  }
 
   newsCard = ({ item }) => (
-      <div className="container mt-2">
-        <CardGroup>
-          <Card body outline color="secondary">
-            {/* <CardTitle><strong>{item.title}</strong></CardTitle> */}
-            <CardHeader><strong>{item.title}</strong></CardHeader>
-            <CardImg top width="100%" src={item.urlToImage}></CardImg>
-            <CardBody>
-              <CardSubtitle><strong>{item.author}</strong></CardSubtitle>
-              <CardText>{item.content}</CardText>
-              <Button color="dark"><a href={item.url}>READ MORE</a></Button> 
-            </CardBody>
-          </Card>
-        </CardGroup>
-      </div>
+    <div className="container mt-2">
+      <CardGroup>
+        <Card body outline color="secondary">
+          <CardHeader>
+            <strong>{item.title}</strong>
+          </CardHeader>
+          <CardImg top width="100%" src={item.urlToImage}></CardImg>
+          <CardBody>
+            <CardSubtitle>
+              <strong>{item.author}</strong>
+            </CardSubtitle>
+            <CardText>{item.content}</CardText>
+            <Button color="dark">
+              <a href={item.url}>READ MORE</a>
+            </Button>
+          </CardBody>
+        </Card>
+      </CardGroup>
+    </div>
   );
 
   render() {
-
-     let res;
+    let res;
     if (this.state.data.length > 0) {
-      console.log(this.state.data);
-
       res = (
         <div>
-        <h1 className="case">{this.props.match.params.type}</h1><hr />
-        {/* <><button onClick={refreshPage}></button></> */}
-        <Row>
-          {this.state.data.map((item, index) => (
-            <div key={index} className="col-12 col-md-5 m-auto">
-              <div >
-                <this.newsCard item={item} />
+          <h1>{this.props.match.params.type}</h1>
+          <Row>
+            {this.state.data.map((item, index) => (
+              <div key={index} className="col-12 col-md-5 m-auto">
+                <div>
+                  <this.newsCard item={item} />
+                </div>
               </div>
-            </div>
-          ))}
-        </Row>
+            ))}
+          </Row>
         </div>
       );
     } else {
       res = <h1>{this.state.errMess}</h1>;
     }
-    return (
-    <div >
-      {res}
-    </div>
-    );
+    return <div className="container">{res}</div>;
   }
 }
 
